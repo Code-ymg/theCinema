4aab7b5f2d9e80f49344a9db2109f2d1792a58be
Code-ymg <yashrajsinh.02.06@gmail.com>
2025-06-10 18:00:04 +0530

    The paths are changed

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -4,14 +4,14 @@ const MovieCard = ({movie :
     { title, vote_average, poster_path, release_date, original_language }}) => {
   return (
     <div className='movie-card'>
-      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './src/assets/No-poster.png'} alt={title} />
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './assets/No-poster.png'} alt={title} />
 
       <div className='mt-4'>
         <h3>{title}</h3>
 
         <div className='content'>
             <div className='rating'>
-                <img src="./src/assets/star.svg" alt="star-icon" />
+                <img src="./assets/star.svg" alt="star-icon" />
                 <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
             </div>
 
