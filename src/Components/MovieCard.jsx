585eb97f56a97c5fbc11217bf47275d7e7f3dd81
Code-ymg <yashrajsinh.02.06@gmail.com>
2025-06-10 18:11:09 +0530

    The assets are loaded in public folder

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -4,22 +4,14 @@ const MovieCard = ({movie :
     { title, vote_average, poster_path, release_date, original_language }}) => {
   return (
     <div className='movie-card'>
-<<<<<<< HEAD
-      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './assets/No-poster.png'} alt={title} />
-=======
-      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'src/assets/No-poster.png'} alt={title} />
->>>>>>> 60d6aa4ac8ec34c65949ebe9ef7ab00459596c25
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './public/assets/No-poster.png'} alt={title} />
 
       <div className='mt-4'>
         <h3>{title}</h3>
 
         <div className='content'>
             <div className='rating'>
-<<<<<<< HEAD
-                <img src="./assets/star.svg" alt="star-icon" />
-=======
-                <img src="src/assets/star.svg" alt="star-icon" />
->>>>>>> 60d6aa4ac8ec34c65949ebe9ef7ab00459596c25
+                <img src="./public/assets/star.svg" alt="star-icon" />
                 <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
             </div>
 
